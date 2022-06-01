3fc9d4dde4c11b76ff69762cfa53046f0e30a07d
José Martins da Rosa Júnior <jose.rosa@supermix.com.br>
2022-06-01 14:11:18 -0300

    #2 - Início da criação do mapa

--- src/Components/MapaMonitoramento/index.js
+++ src/Components/MapaMonitoramento/index.js
@@ -1,16 +1,46 @@
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useMemo  } from 'react';
 import { HubConnectionBuilder } from '@microsoft/signalr';
 import Button from '@mui/material/Button';
 import api from '../../services/api';
+import {Map} from 'pigeon-maps';
+import Marker from 'pigeon-marker';
+
+const getProvider = (x, y, z) => `https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/${z}/${x}/${y}.png`;
+
 
 const Monitoramento = () => {
+
+    const mapConfig = {
+        center: [-19.8272144, -43.1629986],
+        zoom: 16
+    };
+
+    
+    const data = {
+        name: '3',
+        values: [-19.8245789, -43.1620343],
+    }
+
     const [ connection, setConnection ] = useState(null);
-    const [locations, setLocations] = useState([]);
+    const [locations, setLocations] = useState([
+        {
+            name: '0',
+            values: [-19.8272083, -43.1629873],
+        },
+        {
+            name: '1',
+            values: [-19.8267341, -43.1629907],
+        },
+        {
+            name: '2',
+            values: [-19.8267341, -43.1629907],
+        },
+]);
 
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
-            .withUrl('https://localhost:44349/monitoramentoHub')
+            .withUrl('https://topmonitorador-api.herokuapp.com/monitoramentoHub')
             .withAutomaticReconnect()
             .build();
 
@@ -33,35 +63,46 @@ const Monitoramento = () => {
         }
     }, [connection]);
 
-    const gerarNotificacao = () => {
-        const retorno = (result) => {
-            console.log("sucesso geração da notificação", result)
-        }
 
-        const erroRetorno = (error) => {
-            console.log("erro aqui",error)
-        }
-        api.post("https://localhost:44349/monitoramento/receber",{
-            latitude: "teste123213",
-            longitude: "oitetste123"
-        }).then(retorno).catch(erroRetorno)
+    const gerarNotificacao = () => {
+        setLocations((locations) => {
+            debugger
+            return [...locations, data]
+        })
     }
+    
+    const PigeonMarkers = useCallback(() => {
+        return (
+            locations.map(marker => (
+                <Marker
+                    key={`marker_${marker.name}`}
+                    anchor={marker.values}
+                    payload={marker.name}
+                    onClick={() => {}}
+                />
+                )
+            )
+        )
+    },[locations]);
 
     return (
-        <>
-            <div>
-                <Button onClick={gerarNotificacao}> Gerar notificacao</Button>
-            </div>
-            {locations.map((loc) => {
-                return(
-                    <div>
-                        <div>{loc?.latitude}</div>
-                        <div>{loc?.longitude}</div>
-                    </div>
-                )
-            })}
+    <>
+        <div>
+            <Button onClick={gerarNotificacao}> Gerar notificacao</Button>
+        </div>
+        <div className="map">
+            <Map
+                width={window.innerWidth}
+                height={600}
+                defaultCenter={mapConfig.center}
+                defaultZoom={mapConfig.zoom}
+                provider={getProvider}
+            >
+                {PigeonMarkers()}
+            </Map>
+        </div>
             
-        </>
+    </>
     );
 };
 
